4fbd4067e5878d43778cf99943e2ed0aeb733b0b
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-04 15:59:21 +0530

    Time: 2 ms (96.64%), Space: 7.1 MB (86.66%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
-    vector<vector<string> > solveNQueens(int n) {
-        vector<vector<string> > res;
-        vector<string> nQueens(n, string(n, '.'));
-        solveNQueens(res, nQueens, 0, n);
+    std::vector<std::vector<std::string> > solveNQueens(int n) {
+        std::vector<std::vector<std::string> > res;
+        std::vector<std::string> nQueens(n, std::string(n, '.'));
+        std::vector<int> flag_col(n, 1), flag_45(2 * n - 1, 1), flag_135(2 * n - 1, 1);
+        solveNQueens(res, nQueens, flag_col, flag_45, flag_135, 0, n);
         return res;
     }
 private:
-    void solveNQueens(vector<vector<string> > &res, vector<string> &nQueens, int row, int &n) {
+    void solveNQueens(std::vector<std::vector<std::string> > &res, std::vector<std::string> &nQueens, std::vector<int> &flag_col, std::vector<int> &flag_45, std::vector<int> &flag_135, int row, int &n) {
         if (row == n) {
             res.push_back(nQueens);
             return;
         }
         for (int col = 0; col != n; ++col)
-            if (isValid(nQueens, row, col, n)) {
+            if (flag_col[col] && flag_45[row + col] && flag_135[n - 1 + col - row]) {
+                flag_col[col] = flag_45[row + col] = flag_135[n - 1 + col - row] = 0;
                 nQueens[row][col] = 'Q';
-                solveNQueens(res, nQueens, row + 1, n);
+                solveNQueens(res, nQueens, flag_col, flag_45, flag_135, row + 1, n);
                 nQueens[row][col] = '.';
+                flag_col[col] = flag_45[row + col] = flag_135[n - 1 + col - row] = 1;
             }
-    }
-    bool isValid(std::vector<std::string> &nQueens, int row, int col, int &n) {
-        //check if the column had a queen before.
-        for (int i = 0; i != row; ++i)
-            if (nQueens[i][col] == 'Q')
-                return false;
-        //check if the 45° diagonal had a queen before.
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
-            if (nQueens[i][j] == 'Q')
-                return false;
-        //check if the 135° diagonal had a queen before.
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
-            if (nQueens[i][j] == 'Q')
-                return false;
-        return true;
     }
 };
