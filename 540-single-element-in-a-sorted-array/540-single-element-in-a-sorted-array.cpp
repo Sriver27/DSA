e2ff539ac75420fdae45785a00f797b59c4353f6
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-14 16:43:33 +0530

    Time: 20 ms (98.38%), Space: 22.3 MB (91.08%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,39 +1,37 @@
-// This approach uses the fact that since the array is sorted, every duplicate element occur adjacently. We can also see that the length of given array must be odd (Why? All element occur twice while only 1 element occurs once).
+// We can apply binary search using slightly different logic as well.
 
-// Let the search range be [L, R]. Now, when we consider the mid element, the array is split into two equal halves on the left and right sides. Then we have following cases -
+// Suppose you are given a sorted array with all elements occur twice. For eg. [1,1,2,2,5,5,7,7,8,8]. So, here we can see that the elements follow the condition - nums[0] == nums[1], nums[2] == nums[3],..., nums[2*k] == nums[2*k+1]. Now, if we insert an element somewhere in the array, the above relation wont be satisfied from that point of insertion. Eg. [1,1,2,2,4,5,5,7,7,8,8] the condition starts to fail from element 4, that is from point where single-occurence element is present.
 
-// nums[mid] == nums[mid+1]:
+// Thus, we can use this observation to apply binary-search to find the starting point from where the above condition starts to fail. This start point is the index of element occuring once. The binary search will be applied as follows -
 
-    // Left Half Length = Right Half Length = Even : In this case, we can be sure that      our answer lies in the right half. How? One element of right half: nums[mid+1]          matches with nums[mid]. So, leaving nums[mid+1] aside, we only have odd number of       elements to pair up with each other in the right half. This means one element           must be such that it cannot be paired with anyone and this is our required answer       that occurs only once. So do L = mid+2 (+2 to skip nums[mid] and nums[mid+1] &          keep search space odd for next iteration)
+/* Initialize the search space as [L, R] = [0, n-1] and repeat the following step till 
+L <= R.*/
 
-    // Left Half Length = Right Half Length = Odd : In this case, we can be sure that       our answer lies in the left half. Again, how? Following same reasoning, one element     of left half: nums[mid+1] matches with nums[mid]. So, leaving nums[mid+1] aside,        the right half consists of even number of elements which can pair up with each          other completely. However, left half only consists odd elements and thus one            element which cannot be paired with anyone is our required answer. So do R = mid-1      (-1 to skip nums[mid] & keep search space odd for next iteration)
+// Find the mid position and check if the above condition nums[2*k] == nums[2*k+1] is satisifed or not.
+// For this, firstly we must ensure that mid is even. If mid is odd, decrease it by 1 to make it even.
+// Then, we need to check if the condition is satisfied or not -
 
-// nums[mid] == nums[mid-1]: We can follow similar reasoning based on array lengths as above -
+    // nums[mid] == nums[mid+1] : The condition is correctly satisfied till index           mid+1. So, the required element must occur somewhere on the right of mid+1. So do L     = mid+2.
 
-    // Left Half Length = Right Half Length = Even : The answer lies in the left half.      How? One element of left half: nums[mid-1] matches with nums[mid]. This leaves          left half with odd number of elements to pair up with each other and one element        that cant be paired with anyone is the required answer. So do R = mid-2
+    // nums[mid] != nums[mid+1] : The condition is not satisfied at this point. So, the     required element must either be nums[mid] or occur somewhere on the left of mid due     to which the relation is not satisfied. Mark nums[mid] as a potential answer and        search if condition is satisfied to the left of mid by doing R = mid-1
 
-    // Left Half Length = Right Half Length = Odd : The answer lies in right half. How?     One element of left half: nums[mid-1] matches with nums[mid]. This leaves left half     with even number of elements which can pair up with each other completely. However,     right half only consists odd elements and thus one element which cannot be paired       with any other is the required answer. So do L = mid+1.
+// Finally, we return the ans which was marked as potential answer during the binary search.
 
-// If neither of above condition are satisfied, then we can be sure that nums[mid] itself is the required element occuring once (since it doesnt match with its neighbours). So we can just return it.
+
+
 
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = size(nums), L = 0, R = n-1, mid;
+        int n = size(nums), L = 0, R = n-1, mid, ans;
         while(L <= R) {
             mid = (L + R) >> 1;
-            bool isHalfEven = (mid-L) % 2 == 0;          // check length of each half is odd or even
-			
-            if(mid+1 < n && nums[mid] == nums[mid+1])    // case: 1
-                if(isHalfEven) L = mid+2;
-                else R = mid-1;
-				
-            else if(mid && nums[mid] == nums[mid-1])     // case: 2
-                if(isHalfEven) R = mid-2;
-                else L = mid+1;
-				
-            else return nums[mid];                       // case: 3
+            if(mid & 1) mid--;                              // make mid even to check for required condition
+            if(mid+1 < n && nums[mid] == nums[mid+1])       // condition satisfied upto mid+1:
+                L = mid+2;                                  // so search in [mid+2, R] to find point where condition starts to fail
+            else                                            // condition not satisfied:
+                R = mid-1, ans = nums[mid];                 // nums[mid] is potential answer. search [L, mid-1] to see if condition started to fail somewhere before
         }
-        return -1;   // wont be reached since we will always find required element inside loop
+        return ans;
     }
 };
