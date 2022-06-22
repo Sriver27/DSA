f7b9b62dd946697995240786430526bdf4b24991
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-22 11:20:49 +0530

    Time: 63 ms (10.77%), Space: 18.5 MB (9.65%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:
+    int kth_Largest(map<int,int> m, int k){
+        int freq = 0;
+	for (auto it = m.begin(); it != m.end(); it++) {
+		freq += (it->second); // adding the frequencies of
+							// each element
+		if (freq >= k) // if at any point frequency becomes
+					// greater than or equal to k then
+					// return that element
+		{
+			return it->first;
+		}
+	}
+	return -1; // returning -1 if k>size of the array which
+			// is an impossible scenario
+    }
     int findKthLargest(vector<int>& nums, int k) {
+        map<int,int> m;
         
-        sort(nums.begin(), nums.end());
-        // for(int i = 0;i<nums.size();i++) cout<<nums[i]<<" ";
+        for(int i =0;i<nums.size();i++){
+            m[nums[i]]+=1;
+        }
         
-        return nums[nums.size()-k];
+        int ans = kth_Largest(m, nums.size()-k+1);
+        
+        return ans;
     }
 };
