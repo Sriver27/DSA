f677fb275b8d35866ba23921aacfa49823b1f58c
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-22 12:11:13 +0530

    Time: 8 ms (12.37%), Space: 5.9 MB (76.15%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,15 +1,45 @@
-class Solution {
-public:
-    int reverse(int x) {
-        int ans=0;
+// class Solution {
+// public:
+//     int reverse(int x) {
+//         int ans=0;
+//         while(x){
+//             if(ans>INT_MAX/10 || ans<INT_MIN/10){
+//                 return 0;
+//             }else{
+//                 ans=ans*10 +x%10;
+//                 x/=10;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution{
+    
+    public:
+
+int reverse(int x) {
+        
+        int upperLimit =  (pow(2,31) - 1)/10;
+        int lowerLimit = -(pow(2,31)/10);
+    
+        int ans = 0;
+        
         while(x){
-            if(ans>INT_MAX/10 || ans<INT_MIN/10){
-                return 0;
-            }else{
-                ans=ans*10 +x%10;
-                x/=10;
+           
+            int lastDigit = x%10;
+			
+            if(ans <= upperLimit && ans >= lowerLimit){ // Checking if ans will be there in integer range only.
+                 int temp = ans * 10 + lastDigit;
+                 ans = temp;
+                 x/=10;  
             }
+            else{ // ans would exceed the int if multiplied by 10. Thus integer overflow occur
+                return 0;
+            }
+            
         }
         return ans;
     }
+    
 };
