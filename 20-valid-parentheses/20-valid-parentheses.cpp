7556abab915017ce140787e2bf67b649fc8b2e8b
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-22 15:32:56 +0530

    Time: 8 ms (6.95%), Space: 6.3 MB (50.88%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,24 +1,28 @@
-class Solution {
-public:
-    bool isValid(string s) {
-        stack<char> myStack;
-        for (char c : s)
-        {
-            if (myStack.empty() || c == '(' || c == '{' || c == '[')
-            {
-                myStack.push(c);
-            }
-            else
-            {
-                if ((c == ')' && myStack.top() != '(') || 
-                    (c == ']' && myStack.top() != '[') ||
-                    (c == '}' && myStack.top() != '{'))
-                {
-                    return false;
-                }
-                myStack.pop();
-            }
+// Great! This is my code inspired by your solution. Given ascii
+
+// '('：40    ')'：41
+// '['：91    ']'：93
+// '{'：123   '}'：125
+// Just use the trick that c/40->1,2,3
+
+class Solution{
+    
+    public:
+
+bool isValid(string s) {
+    char trick[] = { ')', ']', '}' };
+    stack<char> matching;
+    for (auto c : s) {
+        if (c == '(' || c == '[' || c == '{') {
+            matching.push((c / '(') - 1);
+        } else if (matching.empty()) {
+            return false;
+        } else if (c != trick[matching.top()]) {
+            return false;
+        } else {
+            matching.pop();
         }
-        return myStack.empty();
     }
+    return matching.empty();
+}
 };
