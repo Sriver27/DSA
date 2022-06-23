f3d3fcaa52f69fe9d3525cba65208954fdbf9f13
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-23 17:39:32 +0530

    Time: 28 ms (45.36%), Space: 10.6 MB (57.98%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> indices;
-        for (int i = 0; i < nums.size(); i++) {
-            if (indices.find(target - nums[i]) != indices.end()) {
-                return {indices[target - nums[i]], i};
-            }
-            indices[nums[i]] = i;
+        int n = nums.size();
+        vector<pair<int, int>> arr(n);
+        for (int i = 0; i < n; ++i) {
+            arr[i] = make_pair(nums[i], i);
+        }
+        sort(arr.begin(), arr.end()); // Sort arr in increasing order by their values.
+        int left = 0, right = n- 1;
+        while (left < right) {
+            int sum2 = arr[left].first + arr[right].first;
+            if (sum2 == target) 
+                return {arr[left].second, arr[right].second};
+            if (sum2 > target)
+                right -= 1; // Try to decrease sum2
+            else
+                left += 1; // Try to increase sum2
         }
         return {};
     }
