6992809053e4212ab07636fdfa9c5fd55a2d83b8
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-24 09:15:36 +0530

    Time: 766 ms (6.60%), Space: 25.7 MB (10.65%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        
-        if(nums.size()< 3) return 0;
-        
-        int closest = nums[0]+nums[1]+nums[2];
-        sort(nums.begin(),nums.end());
-        
-        for(int first = 0; first < nums.size()-2 ; first++){
-            if(first > 0 && nums[first]== nums[first-1]) continue; // if found a duplicate number, then continue
-            
-            int second = first+1;
-            int third = nums.size()-1;
-            
-            while(second < third){
-                int currSum = nums[first]+nums[second]+nums[third];
-                
-                if(target == currSum) return currSum;
-                
-                if(abs(target-currSum)<abs(target-closest)){
-                    closest = currSum;
-                }
-                
-                if(currSum > target) third--;
-                
-                else second++;
+        sort(nums.begin(), nums.end());
+        int res = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i < nums.size() - 2; i++){
+            int j = i + 1, k = nums.size() - 1;
+            while(j < k){
+                int num = nums[i] + nums[j] + nums[k];
+                if(abs(num - target) < abs(res - target)) res = num;
+                if(num < target) j++;
+                else k--;
             }
         }
-        
-        return closest;
+        return res;
     }
 };
