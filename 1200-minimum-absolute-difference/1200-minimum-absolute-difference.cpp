a379e0425964081621d4680f3557c8e3cbc8baf9
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-27 20:21:34 +0530

    Time: 125 ms (26.76%), Space: 32.2 MB (52.48%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -2,21 +2,19 @@
 
 class Solution {
 public:
-    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        //sort the array using built in method which gives us a TC of O(nlogn)
-        sort(arr.begin(),arr.end());
-        //A pair which has difference and a vector of of two points which stores the min absolute difference
-        pair<int,vector<vector<int>>> result(INT_MAX,{});
-        //Traverse the given array takes O(n) time
-        for(int i=0;i<arr.size()-1;++i) 
-        {
-            //if its not a min absolute difference then use continue and jump to next iteration
-            if(arr[i+1]-arr[i]>result.first) continue;
-            //min diff == diff then add the pair
-            if(arr[i+1]-arr[i]==result.first) result.second.push_back({arr[i],arr[i+1]});
-            //else diff<min diff 
-            else result={arr[i+1]-arr[i],{{arr[i],arr[i+1]}}};    
+    vector<vector<int>> minimumAbsDifference(vector<int>& A) {
+        sort(begin(A), end(A));
+        vector<vector<int>> ans;
+        int minDiff = INT_MAX, curDiff;
+        for(int i = 1; i < size(A); i++) {
+            auto [a, b] = minmax(A[i], A[i-1]);   // a and b will hold min and max amongst A[i] and A[j]
+            curDiff = b - a;
+            if(curDiff == minDiff) ans.push_back({a, b});       // push if equal to MAD found till now
+            else if(curDiff < minDiff) {     // if current diff < MAD, update it and only keep current pair in ans
+                minDiff = curDiff;
+                ans = {{a, b}};
+            }
         }
-        return result.second;
+        return ans;
     }
 };
