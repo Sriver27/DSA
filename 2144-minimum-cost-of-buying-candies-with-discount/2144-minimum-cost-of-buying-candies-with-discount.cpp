6feea7b0b12fcc49db78c99d250087f600a5efd7
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-27 20:46:51 +0530

    Time: 7 ms (62.24%), Space: 10.8 MB (81.22%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -1,11 +1,19 @@
+/*For the max value, we have to pay for it.
+For the second max value, we still have to pay for it.
+For the third max value, we can get it free one as bonus.
+And continuely do this for the rest.
+
+The the core of problem, is need to sort the input.
+All A[i] with i % 3 == n % 3, we can get it for free.*/
+    
 class Solution {
 public:
     int minimumCost(vector<int>& cost) {
         
-        sort(cost.begin(), cost.end()); int minCost = 0;
+        sort(cost.begin(), cost.end()); int minCost = 0, n = cost.size();
         
         for(int i = 0; i< cost.size(); i++){
-            if(i % 3 != cost.size() % 3) minCost+= cost[i];
+            if(i % 3 != n % 3) minCost+= cost[i];
         }
         
         return minCost;
