33ab1d1437b9dfc46a0c939635600088f8f67ce8
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-06-29 17:07:18 +0530

    Time: 19 ms (31.95%), Space: 11.5 MB (45.59%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -1,26 +1,21 @@
- int myCompare(string x, string y){
-        
-        string XY = x.append(y);
-        string YX = y.append(x);
-        
-        return XY.compare(YX)>0 ? 1 : 0 ;
-    }
+ bool compare(string a,string b){
+    return a+b > b+a;
+}
 
 class Solution {
 public:
-   
     string largestNumber(vector<int>& nums) {
-        
+	
         vector<string> container;
+		
+        for(int i : nums)  container.push_back(to_string(i));
         
-        for(int i : nums) container.push_back(to_string(i));
-        
-        sort(container.begin(),container.end(),myCompare);
+        sort(container.begin(),container.end(),compare);
         
         string result;
         
         for(int i=0;i<container.size();i++)  result+=container[i];
         
-        return result[0]=='0'? "0" : result; // checked for when all numbers in num vector are 0
+        return result[0]=='0'? "0" : result;
     }
 };
