43a5b83348d0219c54f2fa3e27d788cbedf55f65
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-09-03 22:04:47 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (100.00%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isStrictlyPalindromic(int n) {
         
-        return false;
+        return false; // returning false in all conditions as there no number exists which satisfies the scrictly palindromic no. 
         
     }
 };
