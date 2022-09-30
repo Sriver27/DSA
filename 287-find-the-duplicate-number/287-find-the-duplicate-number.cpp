5c0f1ce032c31dfcaa943b822dcd2eefffa257eb
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-09-30 09:20:08 +0530

    Time: 207 ms (55.51%), Space: 84 MB (15.36%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,10 +1,16 @@
-// class Solution {
-// public:
+class Solution {
+public:
 //     int findDuplicate(vector<int>& nums) {
         
-//         int ans = 0;
+//         int ans = nums[0];
         
-//         for(int i=0; i<nums.size(); i++)
+//         unordered_map<int,int> mp;
+//         for(int i=0;i<nums.size();i++) mp[nums[i]]++;
+	
+// 	    if(mp.size()==1)
+// 	    return ans;
+	  
+//         for(int i=1; i<nums.size(); i++)
 //         {
 //             ans^=nums[i];
 //         }
@@ -16,29 +22,40 @@
 //             ans^=i;
 //         }
         
-//         cout<<ans;
+       
         
 //         return ans;
         
 //     }
-// };
-
-class Solution {
-public:
-    int findDuplicate(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> v(n+1,0);
-        for(int i=0;i<nums.size();i++)
-        {
-            if(v[nums[i]]!=0)
-            {
-                return nums[i];
-            }
-            else
-            {
-                v[nums[i]]++;
-            }    
-        }
-        return -1;
-    }
+    int findDuplicate(vector<int>& nums){
+     unordered_set<int> s;
+     
+     for(int i=0; i<nums.size(); i++){
+         if(s.find(nums[i])!=s.end()){
+             return nums[i];
+         }
+         s.insert(nums[i]);
+     }
+     return -1;
+ } 
 };
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> v(n+1,0);
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(v[nums[i]]!=0)
+//             {
+//                 return nums[i];
+//             }
+//             else
+//             {
+//                 v[nums[i]]++;
+//             }    
+//         }
+//         return -1;
+//     }
+// };
