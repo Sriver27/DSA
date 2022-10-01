0209fe203de5818d9327c3771afa9d9587ad7a1f
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-10-01 18:39:44 +0530

    Time: 4 ms (75.85%), Space: 11 MB (95.71%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -2,35 +2,66 @@ class Solution {
 public:
     
    
-int search(vector<int>& nums, int target) { 
-        int s=0; 
-        int e=nums.size()-1;
-        while(s<=e){
-            
-          int mid = (s+e)/2;
-            
-          if(nums[mid]==target) return mid;
-            
-          if(nums[mid]>=nums[0]){
-              
-            //-----------left side ---------------------
-          if(target>=nums[s]  && target<nums[mid]){
-              e = mid-1;
-          }
-          else {
-              s = mid+1;
-          }
-          }
-          else{
-              //right half
-              if(target>nums[mid]  && target<=nums[e]){
-                  s = mid+1;
-              }
-              else{
-                  e = mid-1;
-              }
-          }
+int getPivot(vector<int>& arr, int n) {
+
+    int s = 0;
+    int e = n-1;
+    int mid = s + (e-s)/2;
+
+    while(s<e) {
+
+        if(arr[mid] >= arr[0])
+        {
+            s = mid+1;
         }
-          return -1;
+        else{
+            e = mid;
+        }
+        mid = s + (e-s)/2;
+    }
+    return s;
 }
+    
+    int binarySearch(vector<int>& arr, int s, int e, int key) {
+
+    int start = s;
+    int end = e;
+
+    int mid = start + (end-start)/2;
+
+    while(start <= end) {
+
+        if(arr[mid] == key) {
+            return mid;
+        }
+
+        //go to right wala part
+        if(key > arr[mid]) {
+            start = mid + 1;
+        }
+        else{ //key < arr[mid]
+            end = mid - 1;
+        }
+
+        mid = start + (end-start)/2;
+    }
+    
+    return -1;
+}
+
+    
+    int search(vector<int>& nums, int target) {
+        
+        int pivot = getPivot(nums, nums.size());
+        
+        if(target >= nums[pivot] && target <= nums[nums.size()-1]){
+            
+            return binarySearch(nums,pivot,nums.size()-1,target); // target lies on second line
+        
+        }
+        
+        else{
+            return binarySearch(nums,0,pivot-1,target); // target lies on first line
+        }
+    }
 };
