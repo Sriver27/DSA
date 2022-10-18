934750997ca175c0025c94d0893507fbbf7251c8
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-10-18 19:53:29 +0530

    Time: 15 ms (22.88%), Space: 8.8 MB (99.93%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        
+        int i=0;int ansIndex=0;
+        int n = chars.size();
+        
+        while(i<n)
+        {
+            int j = i+1;
+            
+            while(j<n && chars[i]==chars[j])
+            {
+                j++;
+            }
+            
+            // yha tabhi aaoge jab poora vector traverse krliye hoge ya fir koi different character encounter hogyi ho
+            
+            // store karwalo purani character ko
+            chars[ansIndex++] = chars[i];
+            int count = j-i;
+            
+            if(count>1)
+            {
+                string cnt = to_string(count);
+                
+                for(char ch:cnt)
+                {
+                    chars[ansIndex++]=ch;
+                }
+               
+            }
+             i=j;
+        }
+        
+        return ansIndex;
+        
+    }
+};
