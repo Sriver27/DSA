4591f67332aaaac6d0d3a83702a6378336f785c3
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2022-10-25 23:14:22 +0530

    Time: 13 ms (21.77%), Space: 32.1 MB (6.92%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -2,14 +2,40 @@ class Solution {
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
         
-        string a,b;
+//         string a,b;
         
-        for(auto it1 : word1) a+=it1;
+//         for(auto it1 : word1) a+=it1;
         
-        for(auto it2 : word2) b+=it2;
+//         for(auto it2 : word2) b+=it2;
         
-        if(a.compare(b)==0) return 1;
+//         if(a.compare(b)==0) return 1;
         
-        return 0;
+//         return 0;
+        
+        
+        int p1 = 0, p2 = 0; // inner pointer
+	int w1 = 0, w2 = 0; // outer pointer
+
+	while (w1 < word1.size() && w2 < word2.size()) {
+		string curr1 = word1[w1], curr2 = word2[w2];
+
+		if (curr1[p1] != curr2[p2]) return false;
+
+		if (p1 < curr1.length() - 1) {
+			p1++;
+		} else {
+			p1 = 0;
+			w1++;
+		}
+
+		if (p2 < curr2.length() - 1) {
+			p2++;
+		} else {
+			p2 = 0;
+			w2++;
+		}
+	}
+
+	return w1 == word1.size() && w2 == word2.size();
     }
 };
