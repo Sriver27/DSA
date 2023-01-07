548396e33e797ac7d83cdc8c70d00b448af251e4
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2023-01-07 22:51:15 +0530

    Time: 17 ms (68.68%), Space: 22.2 MB (6.43%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,27 +11,38 @@
  */
 class Solution {
 public:
-     int height(TreeNode* node){
-        if(node == NULL) return 0;
+    pair<bool, int> isBalancedFast(TreeNode* root) {
+                // base case
+        if(root == NULL)
+        {
+            pair<bool, int> p = make_pair(true, 0);
+            return p;
+        }
         
-        int left = height(node->left);
-        int right = height(node->right);
+        pair<int,int> left = isBalancedFast(root->left);
+        pair<int,int> right = isBalancedFast(root->right);
         
-        int ans = max(left,right)+1;
         
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+        
+        bool diff = abs (left.second - right.second ) <=1;
+        
+        pair<bool,int> ans;
+        ans.second = max(left.second, right.second) + 1;
+        
+        if(leftAns && rightAns && diff) {
+           ans.first = true;
+        }
+        else
+        {
+            ans.first = false;
+        }
         return ans;
     }
     bool isBalanced(TreeNode* root) {
         
-        if(root == NULL) return true;
-        
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
-        
-        bool diff = abs(height(root->left) - height(root->right)) <= 1;
-        
-        if(left && right && diff) return true;
-        else return false;
+        return isBalancedFast(root).first;
         
     }
 };
