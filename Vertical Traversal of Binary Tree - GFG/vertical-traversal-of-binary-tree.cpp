89b2119ef4f7008ed678b049880b6b44765dc0b6
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2023-01-12 22:22:44 +0530

    Added solution - LeetHub

--- Vertical Traversal of Binary Tree - GFG/vertical-traversal-of-binary-tree.cpp
+++ Vertical Traversal of Binary Tree - GFG/vertical-traversal-of-binary-tree.cpp
@@ -103,7 +103,7 @@ class Solution
     //Function to find the vertical order traversal of Binary Tree.
     vector<int> verticalOrder(Node *root)
     {
-        map<int,map<int,vector<int>>> nodes;
+        /*map<int,map<int,vector<int>>> nodes;
         queue<pair<Node*,pair<int,int>>> q;
         vector<int> ans;
         
@@ -139,6 +139,39 @@ class Solution
             }
         }
         
+        return ans;*/
+        
+        
+        
+        map<int,vector<int>> mp;
+        queue<pair<Node*,int>> q;
+        vector<int> ans;
+        if(!root)
+        return ans;
+        
+        q.push(make_pair(root,0));
+        while(!q.empty())
+        {
+            pair<Node*,int> p = q.front();
+            q.pop();
+            
+            Node *curr = p.first;
+            int hd = p.second;
+            
+            mp[hd].push_back(curr->data);
+            
+            if(curr->left)
+            q.push(make_pair(curr->left,hd-1));
+            if(curr->right)
+            q.push(make_pair(curr->right,hd+1));
+        }
+         for(auto it: mp)
+        {
+            for(auto i : it.second)
+            {
+                ans.push_back(i);
+            }
+        }
         return ans;
     }
 };
