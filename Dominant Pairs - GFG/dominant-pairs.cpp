a15d3f7c1a84eff80db5666b4791138f2d604e9a
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2023-04-12 08:49:58 +0530

    Added solution - LeetHub

---
+++ Dominant Pairs - GFG/dominant-pairs.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    int dominantPairs(int n,vector<int> &arr){
+        // Code here
+        int cnt = 0;
+        sort(arr.begin(),arr.begin()+n/2);
+        sort(arr.begin()+n/2,arr.end());
+        
+        int j = n/2;
+        for(int i = 0; i<n/2; i++)
+        {
+            while(j<n && arr[i]>=arr[j]*5) j++;
+            cnt += (j-n/2);
+        }
+        
+        return cnt;
+    }  
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.dominantPairs(n,arr)<<endl;
+    }
+}
+// } Driver Code Ends
