1689afd568732d49cf7c60846cf492aebfd9e8fd
Harsh Jain <75061992+Sriver27@users.noreply.github.com>
2023-07-11 18:43:07 +0530

    Time: 0 ms (100.00%), Space: 7 MB (91.05%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,16 +11,15 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        /*Jab tak fast pointer end mein pahuchega tab tak slow mid tak hi pahuchega kyoki, fast pointer head node se start kar 2x mei move kar rh hai and slow vhi sirf 1x mein*/
         
-        if(head == NULL) return head;
+        if(head == NULL || head -> next == NULL) return head;
         
-        if(head->next == NULL) return head; // if the list has just 1 node, then vhi na mid hoga
         ListNode* fast = head;
         ListNode* slow = head;
         
-        while(fast != NULL && fast->next != NULL){
-            fast = fast->next->next;
+        while(fast != NULL && fast->next != NULL) 
+        {
+            fast= fast->next->next;
             slow = slow->next;
         }
         
